e3940b71628704025cbb91643e8bed2e84bd1f18
Pervez <pervezhossain1998@gmail.com>
2023-06-22 15:31:55 +0600

    Code Added

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-'use strict';
+"use strict";
 
-// // Getting The Text Element
-// document.querySelector(".message").textContent
-
-// // Setting the text content
-// document.querySelector('.message').textContent = "Correct Answer"
-// document.querySelector('.number').textContent = 13
-// document.querySelector('.score').textContent = 20
-// document.querySelector('.guess').value = 20
-
-let mainValue = document.querySelector('.number').value = Math.floor(Math.random() * 20) + 1
-// document.querySelector('.number').textContent = mainValue
-
+let mainValue = Math.floor(Math.random((document.getElementById('guessNum').textContent)) * 20 + 1)
+// document.getElementById('guessNum').innerHTML = mainValue
 let score = 20;
 let highScore = 0
-// console.log();
 
-document.getElementById('btnCheck').addEventListener('click', function () {
+const textMessage = function(message){
+    return document.querySelector(".message").innerHTML = message
+}
+
+
+
+document.getElementById('btnCheck').addEventListener('click', function(){
+    
     const guessValue = Number(document.querySelector(".guess").value)
+    
+    if(!guessValue) {
+        textMessage("Please Choice A Number")
+    }
 
-    if (!guessValue) {
-        document.querySelector('.message').textContent = "No Number"
-    } else if (guessValue === mainValue) {
-        document.querySelector('.message').textContent = "Correct Answer"
-        document.querySelector('body').style.backgroundColor = "green"
-        document.querySelector('.number').style.width = "30rem"
-        document.querySelector(".number").textContent = mainValue
+    if(guessValue > mainValue){
+        textMessage("Your Guess Is Too High")
+    }else if(guessValue < mainValue){
+        textMessage("Your Guess Is Too Low")
         
-        if (score > highScore) {
+    } else {
+        textMessage("Congratulations! You Win The Game")
+        document.getElementById('guessNum').innerHTML = mainValue
+        document.querySelector('.score').textContent = score
+        document.querySelector('body').style.backgroundColor = "green"
+        document.getElementById("guessNum").style.width = "30rem"
+        
+        if(score > highScore){
             highScore = score
             document.querySelector(".highscore").textContent = highScore
         }
 
-    } else if (guessValue < mainValue) {
-        document.querySelector('.message').textContent = "Too Low"
-    } else if (guessValue > mainValue) {
-        document.querySelector('.message').textContent = "Too High"
     }
 
-
-
-    if (guessValue < mainValue || guessValue > mainValue) {
-        document.querySelector(".score").textContent = score - 1
+    if(guessValue > mainValue || guessValue < mainValue){
+        // score - 1
+        document.querySelector('.score').textContent = score - 1
         score--
     }
-
-    if (score < 0) {
-        document.querySelector('.message').textContent = "You Loose The Game"
-        document.querySelector(".score").textContent = 0
+    
+    if(score < 0){
+        score = 0
+        document.querySelector('.score').textContent = score
+        textMessage("You Loose The Game")
     }
 })
 
-document.querySelector(".again").addEventListener("click", function () {
-    score = 20;
-    document.querySelector(".number").textContent = "?"
-    mainValue = Math.floor(Math.random() * 20) + 1
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').innerHTML = "?"
     document.querySelector('body').style.backgroundColor = "#222"
-    document.querySelector('.number').style.width = "15rem"
-    document.querySelector('.message').textContent = "Start Guessing..."
-    document.querySelector(".guess").value = " "
-    document.querySelector(".score").textContent = score
+    document.getElementById("guessNum").style.width = "15rem"
+    mainValue = Math.floor(Math.random((document.getElementById('guessNum').textContent)) * 20 + 1)
+    textMessage("Start Guessing...")
+
 
 })
